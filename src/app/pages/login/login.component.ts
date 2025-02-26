18fa51bcd77fb1776b9ca30250061b0f564e356b
Kamil Nawrot <kamilnawrot98@gmail.com>
2025-02-26 07:25:45 +0100

    custom control logic, basic error handling mechanism

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { InputControlComponent } from '../../shared/components/input-control/input-control.component';
+import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   imports: [
-    InputControlComponent
+    InputControlComponent,
+    ReactiveFormsModule,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  private readonly fb = inject(FormBuilder);
+
+  form = this.fb.group<{ email: FormControl<string>, password: FormControl<string> }>({
+    email: this.fb.nonNullable.control('', Validators.required),
+    password: this.fb.nonNullable.control('', Validators.required),
+  })
+
+  protected onLogIn(event: any): void {
+    console.log(this.form.value);
+  }
+
+  ngOnInit(): void {
+    this.form.controls.email.valueChanges.subscribe(value => {
+      console.log('email:', value);
+    })
+  }
 
 }
--- src/app/shared/components/input-control/input-control.component.ts
+++ src/app/shared/components/input-control/input-control.component.ts
@@ -1,12 +1,83 @@
-import { Component, input } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  forwardRef,
+  Injector,
+  input,
+  Optional,
+  Self
+} from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  FormControl, FormControlName, FormsModule,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-input-control',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './input-control.component.html',
-  styleUrl: './input-control.component.scss'
+  styleUrl: './input-control.component.scss',
+  providers: [
+    { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => InputControlComponent), multi: true },
+  ]
 })
-export class InputControlComponent {
+export class InputControlComponent implements ControlValueAccessor, AfterContentInit {
   label = input('');
   type = input<'text' | 'password'>('text');
+
+  control!: AbstractControl<string>;
+  value = '';
+  touched = false;
+  disabled = false;
+
+  onChange = (value: string) => {}
+  onTouched = () => {}
+
+  protected readonly Validators = Validators;
+
+  constructor(@Optional() @Self() private injector: Injector) {}
+
+  ngAfterContentInit(): void {
+    const control = this.injector.get(FormControlName);
+    if (control) {
+      console.log('CONTROL:', control, 'VALUE:', this.value);
+      this.control = control.control;
+    }
+  }
+
+  writeValue(value: string) {
+    console.log('value:', value);
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onValueChange(event: any): void {
+    this.onChange(event.target.value);
+    this.markAsTouched();
+  }
+
+  private markAsTouched(): void{
+    if(!this.touched){
+      this.touched = true;
+      this.onTouched();
+    }
+  }
+
+  protected readonly FormControl = FormControl;
 }
