165bb99a8e39d374be744803c99faba84ebcfecb
Kamil Nawrot <kamilnawrot98@gmail.com>
2025-02-27 10:05:38 +0100

    added page titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { signedInGuard, signedOutGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent, canActivate: [signedOutGuard] },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [signedInGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [signedOutGuard], title: 'AppVerk Recruitment Task - Sign In' },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [signedInGuard], title: 'AppVerk Recruitment Task - Dashboard' },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
